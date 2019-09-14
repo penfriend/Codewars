5b4d1afa1563f067b0b3a947e089620f6b8405b7
penfriend <dianapenfriend@gmail.com>
2019-09-14 03:03:10 +0300

    Task "even or odd"

---
+++ even_or_odd.js
@@ -0,0 +1,7 @@
+/**
+ * @link: https://www.codewars.com/kata/even-or-odd
+ * @Task: Create a function (or write a script in Shell) that takes an integer 
+ * as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ * @Author: Diana Holik
+ */
+const even_or_odd = number => number % 2 ? 'Odd' : 'Even';
