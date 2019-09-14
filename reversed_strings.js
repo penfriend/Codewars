e48d46c3f7f5b78fc28db20686fc0f8fcb260131
penfriend <dianapenfriend@gmail.com>
2019-09-14 03:18:34 +0300

    Task "reversed strings"

---
+++ reversed_strings.js
@@ -0,0 +1,7 @@
+/**
+ * @link: https://www.codewars.com/kata/reversed-strings
+ * @Task: Complete the solution so that it reverses the string value passed into it.
+reversed('world'); // returns 'dlrow'
+ * @Author: Diana Holik
+ */
+const reversed = s => s.split('').reverse().join('');
